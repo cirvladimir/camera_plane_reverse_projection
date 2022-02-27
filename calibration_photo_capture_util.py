d1cd2d1bb5b17a5594c20532bc8ed1f8e7a72778
Vladimir Korukov <vladimirkorukov@gmail.com>
2022-02-26 22:40:51 -0800

    Camera intrinsic calibration done.

--- calibration_photo_capture_util.py
+++ calibration_photo_capture_util.py
@@ -20,7 +20,7 @@ parser.add_argument("--calibration_grid_width", default=8, type=int,
 parser.add_argument("--calibration_grid_height", default=10, type=int,
                     help="Number of squares along the height of the calibration grid.")
 
-parser.add_argument("--calibration_sharpness_threshold", default=2000, type=int,
+parser.add_argument("--calibration_sharpness_threshold", default=1500, type=int,
                     help="Discard images which are less sharp than this value. Higher number discards more images, 0 turns feature off.")
 
 
@@ -39,6 +39,9 @@ parser.add_argument("--y2", default=477.5, type=float,
 parser.add_argument("--num_photos", default=20, type=int,
                     help="Number of camera distortion calibration photos.")
 
+parser.add_argument("--start_delay", default=0, type=float,
+                    help="Number of seconds to wait at the start.")
+
 args = parser.parse_args()
 
 vid = cv2.VideoCapture(args.video_capture_device_index)
@@ -50,6 +53,7 @@ _, last_frame = vid.read()
 
 new_frame = last_frame
 stop_camera = False
+show_camera = False
 
 
 def new_frame_updater():
@@ -58,6 +62,10 @@ def new_frame_updater():
     _, frame = vid.read()
     if frame is not None:
       new_frame = frame
+
+    if show_camera:
+      cv2.imshow("image", new_frame)
+      cv2.waitKey(10)
 
     if stop_camera:
       vid.release()
@@ -73,12 +81,17 @@ grid_points = np.zeros(
     (1, checkerboard_size[0] * checkerboard_size[1], 3), np.float32)
 grid_points[0, :, :2] = np.mgrid[0:checkerboard_size[0],
                                  0:checkerboard_size[1]].T.reshape(-1, 2)
+# Creating vector to store vectors of 3D points for each checkerboard image
+world_points = []
+# Creating vector to store vectors of 2D points for each checkerboard image
+image_points = []
 
 
 def process_frame(img):
   # Returns true if image was good.
   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+  print("Looking for grid in image...")
   ret, corners = cv2.findChessboardCorners(
       gray, checkerboard_size, cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
 
@@ -93,33 +106,49 @@ def process_frame(img):
                    int(min(np_corners[:, :, 0])[0]):int(max(np_corners[:, :, 0])[0]),
                    :]
 
-  if cv2.Laplacian(grid_image, cv2.CV_64F).var() < args.calibration_sharpness_threshold:
-    print("Image is blurry, discarding. See calibration_sharpness_threshold flag.")
+  image_sharpness = cv2.Laplacian(grid_image, cv2.CV_64F).var()
+  if image_sharpness < args.calibration_sharpness_threshold:
+    print(
+        f"Image is blurry, discarding. Sharpness: {image_sharpness} See calibration_sharpness_threshold flag.")
     return False
+
+  world_points.append(grid_points)
+  # refining pixel coordinates for given 2d points.
+  refined_corners = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1),
+                                     (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001))
+
+  image_points.append(refined_corners)
+
   return True
-
-  # world_points.append(grid_points)
-  # # refining pixel coordinates for given 2d points.
-  # refined_corners = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1),
-  #                                    (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001))
-
-  # image_points.append(refined_corners)
 
 
 num_calibration_images_processed = 0
 
+time.sleep(args.start_delay)
+
+print("Press any key to start.")
+cv2.imshow("image", last_frame)
+cv2.waitKey(0)
+
+show_camera = True
+
 while num_calibration_images_processed < args.num_photos:
+  for i in range(5):
+    print(5 - i)
+    time.sleep(1)
   last_frame = new_frame
-  print("...")
-  cv2.imshow("image", last_frame)
 
   if process_frame(last_frame):
     num_calibration_images_processed += 1
     print(f"{num_calibration_images_processed}/{args.num_photos} images processed")
 
-  cv2.waitKey(0)
-  time.sleep(2)
+print("Image capturing done, calibrating...")
+ret, camera_matrix, distortion, _, _ = cv2.calibrateCamera(
+    world_points, image_points, (last_frame.shape[1],
+                                 last_frame.shape[0]), None, None)
 
+print(camera_matrix)
+print(distortion)
 
 cv2.destroyAllWindows()
 
